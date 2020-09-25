e2ebd934c2d433d4d71060979b268b830e9879de
jyotirsai <jyotirsai28@gmail.com>
2020-09-24 22:42:35 -0600

    plot working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
 
 const App = () => {
   const [data, setData] = useState([]);
+  const [plotData, setPlotData] = useState([]);
 
   useEffect(() => {
     fetch(
@@ -11,11 +13,25 @@ const App = () => {
       .then((data) => setData(data["Time Series (5min)"]));
   }, []);
 
-  Object.keys(data).forEach((key) => {
-    console.log(key, data[key]["4. close"]);
+  let array = [];
+
+  Object.entries(data).forEach((entry) => {
+    const [key, value] = entry;
+    const obj = { Time: key, Price: value["4. close"] };
+    array.push(obj);
   });
 
-  return <div>hi</div>;
+  console.log(array);
+  const renderLineChart = (
+    <LineChart width={800} height={600} data={array}>
+      <Line type="monotone" dataKey="Price" stroke="#8884d8" />
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis domain={[116, 120]} />
+    </LineChart>
+  );
+
+  return <div>{renderLineChart}</div>;
 };
 
 export default App;
